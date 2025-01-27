987e5dec83b8bec276de50af119dcf2cc366de34
Christian Kongsgaard <christian@kongsgaard.eu>
2025-01-27 13:49:03 +0100

    ran checks

--- apps/lcax-landing/src/components/Theme/Theme.ts
+++ apps/lcax-landing/src/components/Theme/Theme.ts
@@ -3,6 +3,6 @@ import { uiTheme } from '@lcax/ui'
 
 const themeOverride = createTheme({
   defaultRadius: 0,
-});
+})
 
-export const theme = mergeMantineTheme(uiTheme, themeOverride);
+export const theme = mergeMantineTheme(uiTheme, themeOverride)
