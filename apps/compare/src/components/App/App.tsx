a0ba9a76955eae7a4bdce2ce62b15ee04ef66aab
Christian Kongsgaard <christian@kongsgaard.eu>
2025-02-18 18:09:24 +0100

    cleaned up packages

--- apps/compare/src/components/App/App.tsx
+++ apps/compare/src/components/App/App.tsx
@@ -2,6 +2,8 @@ import { MantineProvider } from '@mantine/core'
 import { theme, AppRouter } from '@/components'
 import { ProjectProvider } from '@/contexts'
 import { BrowserRouter } from 'react-router'
+import '@mantine/core/styles.css'
+import '@mantine/charts/styles.css'
 
 export const App = () => {
   return (
--- apps/compare/src/main.tsx
+++ apps/compare/src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from '@/components'
-import '@mantine/core/styles.css'
-import '@mantine/charts/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
