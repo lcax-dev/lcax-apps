29982a9a31e6120e389cca1a2f24bd2d0c0c53d7
Christian Kongsgaard <christian@kongsgaard.eu>
2026-09-02 17:12:21 +0200

    Standardize string quotes in `auth.ts` and adjust indexing for consistency.

--- services/lcax/src/models/auth.ts
+++ services/lcax/src/models/auth.ts
@@ -43,7 +43,7 @@ export const account = pgTable(
   'account',
   {
     id: text('id').primaryKey(),
-    issuer: text("issuer").notNull(),
+    issuer: text('issuer').notNull(),
     accountId: text('account_id').notNull(),
     providerId: text('provider_id').notNull(),
     userId: text('user_id')
@@ -62,11 +62,8 @@ export const account = pgTable(
       .notNull(),
   },
   (table) => [
-    uniqueIndex("account_issuer_accountId_uidx").on(
-      table.issuer,
-      table.accountId,
-    ),
-    index("account_userId_idx").on(table.userId),
+    uniqueIndex('account_issuer_accountId_uidx').on(table.issuer, table.accountId),
+    index('account_userId_idx').on(table.userId),
   ],
 )
 
